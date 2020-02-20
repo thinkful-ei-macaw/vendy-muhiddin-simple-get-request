3a5093cd571ac4f2c2775e176d6c67d024583783
Vendy Prum <vendy@thinkful.com>
2020-02-20 11:28:27 -0800

    exercise2

--- simple_get/ex2/index.js
+++ simple_get/ex2/index.js
@@ -1,37 +1,34 @@
-
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage(value) {
+  
+  fetch(`https://dog.ceo/api/breeds/image/random/${value}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img" height="200" width="200">`
-  )
+  $('#imageHolder').html(
+    responseJson.message.map(image => `<img src="${image}" class="results-img">`)
+  );
   //display the results section
   $('.results').removeClass('hidden');
 }
+
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     let value = $('#textInput').val();
-    //console.log(value);
-    for(let i = 0; i < value; i++) {
-      getDogImage();
-
-    }
+    getDogImage(value);
   });
 }
-
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
-  //render();
+
 });
